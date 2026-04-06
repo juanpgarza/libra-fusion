a72006b2a1871b0883a7db0069d83cfb12337c8b
Juan Pablo Garza <juanp@juanpgarza.com>
2026-04-06 11:49:51 -0300

    campos requeridos

--- libra_pronto/models/product_template.py
+++ libra_pronto/models/product_template.py
@@ -21,14 +21,20 @@ class ProductTemplate(models.Model):
     @api.model
     def default_get(self, fields):
         rec = super(ProductTemplate, self).default_get(fields)
-        rec['excluir_calculo_markup'] = False        
-
+        rec['excluir_calculo_markup'] = False
+        rec['uom_id'] = False
+        rec['uom_po_id'] = False
+        rec['categ_id'] = False
+        rec["route_ids"] = False
         return rec
 
     @api.model_create_multi
     def create(self, values):
 
         for val in values:
+
+            if not val.get('default_code'):
+                raise UserError("Debe informar la referencia interna")
 
             # Validate 'type' and 'pack_ok' fields
             if 'type' in val and 'pack_ok' in val:
@@ -56,6 +62,18 @@ class ProductTemplate(models.Model):
                         mensaje_validacion += "- Impuestos cliente \n"
                     if not val.get('supplier_taxes_id'):
                         mensaje_validacion += "- Impuestos de proveedor \n"
+                    if not val.get('sale_additional_description'):
+                        mensaje_validacion += "- Descripción adicional - ecommerce \n"
+                    if val.get('weight') == 0:
+                        mensaje_validacion += "- Peso \n"
+                    if val.get('volume') == 0:
+                        mensaje_validacion += "- Volumen \n"
+                    if not val.get('image_1920'):
+                        mensaje_validacion += "- imagen del producto \n"
+                    if not val.get('seller_ids'):
+                        mensaje_validacion += "- Proveedor \n"
+                    if not val.get('route_ids'):
+                        mensaje_validacion += "- Rutas \n"
 
                 if mensaje_validacion:
                     raise ValidationError(
@@ -81,13 +99,35 @@ class ProductTemplate(models.Model):
                 for rec in self:
                     mensaje_validacion = ""
                     if rec.sale_ok and rec.type == 'consu':
-
-                        if  not rec.barcode:
+                        if not rec.default_code:
+                            raise UserError("Debe informar el campo referencia")
+                        if not rec.barcode:
                             mensaje_validacion += "- codigo de barras \n"
+                        if not rec.excluir_calculo_markup:
+                            mensaje_validacion += "- Excluir del cálculo del markup \n"
                         if not rec.taxes_id:
                             mensaje_validacion += "- Impuestos de cliente \n"
                         if not rec.supplier_taxes_id:
                             mensaje_validacion += "- Impuestos de proveedor \n"
+                        if not rec.sale_additional_description:
+                            mensaje_validacion += "- Descripción adicional - ecommerce \n"
+                        if rec.weight == 0:
+                            mensaje_validacion += "- Peso \n"
+                        if rec.volume == 0:
+                            mensaje_validacion += "- Volumen \n"
+                        if not rec.image_1920:
+                            mensaje_validacion += "- imagen del producto \n"
+
+                        proveedores = rec.seller_ids
+                        if not proveedores:
+                            mensaje_validacion += "- Proveedor \n"
+                        else:
+                            proveedor = proveedores[0]
+                            if proveedor.price == 0:
+                                mensaje_validacion += "- el precio en el proveedor \n"
+
+                        if not rec.route_ids:
+                            mensaje_validacion += "- Rutas \n"
 
                         if mensaje_validacion:
                             detalle_mensaje = mensaje_validacion
