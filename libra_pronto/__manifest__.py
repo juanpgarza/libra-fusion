ba6672409846db773fe5e35bbe0d8007e9096c05
Juan Pablo Garza <juanp@juanpgarza.com>
2026-05-13 09:30:53 -0300

    reporte meses de stock

--- libra_pronto/__manifest__.py
+++ libra_pronto/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Libra Pronto",
     "summary": "",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "category": "",
     "website": "https://github.com/juanpgarza/libra-fusion",
     "author": "juanpgarza",
