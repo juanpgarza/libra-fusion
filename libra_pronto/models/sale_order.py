f407c97bab84e17cf166dba01ea04defbe543b96
Juan Pablo Garza <juanp@juanpgarza.com>
2026-05-26 10:51:36 -0300

    permiso para cancelar pedido de venta

--- libra_pronto/models/sale_order.py
+++ libra_pronto/models/sale_order.py
@@ -68,3 +68,12 @@ class SaleOrder(models.Model):
         super(SaleOrder, self)._action_confirm()
         for picking in self.picking_ids:
             self.env['procurement.group'].run_smart_scheduler(picking.id)            
+
+    def action_cancel(self):        
+        # solo para pedidos de venta. NO incluye presupuestos
+        for rec in self.filtered(lambda x:x.state in ('done','sale') and x.state != 'cancel'):
+            group = "libra_pronto.group_cancel_sale_order"
+            if not self.env.user.has_group(group):
+                group_id = self.env.ref(group)
+                raise ValidationError("Opción habilitada solo para los miembros del grupo: \n\n'{} / {}'".format(group_id.sudo().category_id.name,group_id.name))
+        return super(SaleOrder, self).action_cancel()
